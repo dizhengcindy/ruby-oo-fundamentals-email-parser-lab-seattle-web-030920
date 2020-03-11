7e02d65003bb78e3ebb248e4e36f615eae6f9326
Di Zheng <dizhengcindy@gmail.com>
2020-03-11 04:13:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,9 @@ class EmailAddressParser
   attr_accessor :emails
   
   def parse
-   emails.split.collect{|email| email.split(', ')}
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
    
   end
 end
